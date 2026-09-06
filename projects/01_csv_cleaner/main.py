323901fd3fec1aee7aa0042c1d32039f549759f1
Daniel Alen <danielalen291102@gmail.com>
2026-09-06 13:36:51 +0200

    Practice branch change

--- projects/01_csv_cleaner/main.py
+++ projects/01_csv_cleaner/main.py
@@ -10,14 +10,19 @@ def main():
 
     required_fields = ['name', 'email', 'country']
 
-    list_rows = csv_utils.load_csv(csv_to_clean)
-    cleaned_rows = csv_utils.clean_rows(list_rows, required_fields)
+    list_rows = csv_utils.load_csv(
+        csv_to_clean=csv_to_clean,
+    )
+    cleaned_rows = csv_utils.clean_rows(
+        rows=list_rows,
+        required_fields=required_fields,
+    )
 
     csv_utils.save_cleaned_csv(
-        csv_cleaned,
-        required_fields,
-        cleaned_rows,
-        list_rows,
+        csv_cleaned=csv_cleaned,
+        required_fields=required_fields,
+        cleaned_rows=cleaned_rows,
+        rows=list_rows,
         )
 
 if __name__ == '__main__':
