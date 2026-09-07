fe0e7a6d74497263806d35a9be8afc09da5f222c
Daniel Alen <danielalen291102@gmail.com>
2026-09-07 20:12:56 +0200

    Rename Path variable in Sales Report

--- projects/06_pandas_sales/sales_report/main.py
+++ projects/06_pandas_sales/sales_report/main.py
@@ -4,11 +4,11 @@ import excel_utils
 
 
 def main():
-    main_folder_path = Path('projects/06_pandas_sales/sales_report')
+    project_folder = Path('projects/06_pandas_sales/sales_report')
 
-    csv_to_clean = main_folder_path / 'pandas_sales_practice.csv'
-    report_path = main_folder_path / 'sales_report.xlsx'
-    csv_files_folder = main_folder_path / 'sales_report_csv'
+    csv_to_clean = project_folder / 'pandas_sales_practice.csv'
+    report_path = project_folder / 'sales_report.xlsx'
+    csv_files_folder = project_folder / 'sales_report_csv'
 
     excel_utils.clean_csv_save_worksheets(
         csv_to_clean=csv_to_clean,
