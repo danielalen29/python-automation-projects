45cf547d5b9404a564aba43be9a4548eb66ccaa2
Daniel Alen <danielalen291102@gmail.com>
2026-09-07 19:21:30 +0200

    Add CSV export to Multiple Sales Cleaner

--- projects/06_pandas_sales/multiple_sales_cleaner/main.py
+++ projects/06_pandas_sales/multiple_sales_cleaner/main.py
@@ -3,13 +3,19 @@ from pathlib import Path
 import utils
 
 def main():
-    input_folder = Path(
-        'projects/06_pandas_sales/multiple_sales_cleaner/sales_data'
+    parent_folder = Path(
+        'projects/06_pandas_sales/multiple_sales_cleaner'
     )
+
+    input_folder = parent_folder / 'sales_data'
 
     csv_files = utils.group_files(path_folder=input_folder)
 
-    print(utils.concatenate_files(csv_files=csv_files))
+    combined_dataframe = utils.concatenate_files(csv_files=csv_files)
+
+    output_file = parent_folder / 'combined_sales.csv'
+
+    combined_dataframe.to_csv(output_file, index=False)
 
 if __name__ == '__main__':
     main()
